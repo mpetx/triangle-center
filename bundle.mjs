ba820f763732aa71faf83b91cba791938ae6d84d
mpetx <143336693+mpetx@users.noreply.github.com>
2024-01-12 17:54:26 +0900

    重心を追加

--- bundle.mjs
+++ bundle.mjs
@@ -24,6 +24,19 @@ class Triangle {
     get edgeA() { return [this.vertexB, this.vertexC]; }
     get edgeB() { return [this.vertexC, this.vertexA]; }
     get edgeC() { return [this.vertexA, this.vertexB]; }
+
+    fromBarycentric(a, b, c) {
+	const sum = a + b + c;
+	return [
+	    (a * this.vertexAX + b * this.vertexBX + c * this.vertexCX) / sum,
+	    (a * this.vertexAY + b * this.vertexBY + c * this.vertexCY) / sum
+	];
+    }
+
+    get centroid() { return this.fromBarycentric(1, 1, 1); }
+    get medianA() { return [this.vertexA, this.fromBarycentric(0, 1, 1)]; }
+    get medianB() { return [this.vertexB, this.fromBarycentric(1, 0, 1)]; }
+    get medianC() { return [this.vertexC, this.fromBarycentric(1, 1, 0)]; }
     
 }
 
@@ -37,6 +50,17 @@ const initializeTriangle = () => {
     triangle.vertexBX = rect.width * 0.3;
     triangle.vertexCX = rect.width * 0.7;
     triangle.vertexBY = triangle.vertexCY = rect.height * 0.8;
+    triangleConfigurations.forEach((config) => {
+	const colorPicker = document.getElementById(config[0]);
+	config[1].forEach((config1) => {
+	    const visibilityCheckbox = document.getElementById(config1[0]);
+	    config1[2].forEach((id) => {
+		const elt = document.getElementById(id);
+		elt.setAttribute(config1[1], colorPicker.value);
+		elt.setAttribute("visibility", visibilityCheckbox.checked ? "visible" : "hidden");
+	    });
+	});
+    });
     updateTriangle();
 };
 
@@ -61,6 +85,13 @@ const updateTriangle = () => {
     setSegmentPosition("triangle__edge-a", triangle.edgeA);
     setSegmentPosition("triangle__edge-b", triangle.edgeB);
     setSegmentPosition("triangle__edge-c", triangle.edgeC);
+    if (document.getElementById("centroid-config__centroid").checked)
+	setPointPosition("triangle__centroid", triangle.centroid);
+    if (document.getElementById("centroid-config__median").checked) {
+	setSegmentPosition("triangle__median-a", triangle.medianA);
+	setSegmentPosition("triangle__median-b", triangle.medianB);
+	setSegmentPosition("triangle__median-c", triangle.medianC);
+    }
 };
 
 const triangleVertexMouseEnterListener = (e) => {
@@ -93,7 +124,33 @@ const triangleVertexMouseUpListener = (e) => {
     document.getElementById("triangle-canvas").removeEventListener("mousemove", canvasMouseMoveListeners[e.target.id]);
 };
 
+const createColorPickerInputListener = (config) => (e) => {
+    config.forEach((config1) => {
+	config1[2].forEach((id) => {
+	    document.getElementById(id).setAttribute(config1[1], e.target.value);
+	});
+    });
+};
+
+const createVisibilityCheckboxInputListener = (ids) => (e) => {
+    ids.forEach((id) => {
+	document.getElementById(id).setAttribute("visibility", e.target.checked ? "visible" : "hidden");
+    });
+    if (e.target.checked)
+	updateTriangle();
+};
+
 const triangleVertexIds = ["triangle__vertex-a", "triangle__vertex-b", "triangle__vertex-c"];
+const triangleConfigurations = [
+    [
+	"centroid-config__color",
+	[
+	    ["centroid-config__centroid", "fill", ["triangle__centroid"]],
+	    ["centroid-config__median", "stroke", ["triangle__median-a", "triangle__median-b", "triangle__median-c"]]
+	]
+    ]
+	
+];
 
 window.addEventListener("load", () => {
     initializeTriangle();
@@ -103,5 +160,12 @@ window.addEventListener("load", () => {
 	elt.addEventListener("mouseleave", triangleVertexMouseLeaveListener);
 	elt.addEventListener("mousedown", triangleVertexMouseDownListener);
 	elt.addEventListener("mouseup", triangleVertexMouseUpListener);
+    });
+    triangleConfigurations.forEach((config) => {
+	const colorPicker = document.getElementById(config[0]);
+	colorPicker.addEventListener("input", createColorPickerInputListener(config[1]));
+	config[1].forEach((config1) => {
+	    document.getElementById(config1[0]).addEventListener("input", createVisibilityCheckboxInputListener(config1[2]));
+	});
     });
 });
